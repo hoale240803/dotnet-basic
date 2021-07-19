8fcb173ccc68324eec9dfd8e9ea60ab573023499
lehoa240119999 <hoale240803@gmail.com>
2021-07-19 11:51:44 +0700

    out & ref, linq

---
+++ CS008_Out&Ref/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace CS008_Out_Ref
+{
+    // interface Animals
+    // {
+    //     void bake();
+    // }
+    // class Program
+    // {
+    //     static void Main(string[] args)
+    //     {
+    //         int a;
+    //         //Console.WriteLine("Gia tri cua a la: " +a);
+    //         DemoOut(out a);
+    //         Console.WriteLine("Gia tri cua a bay gio la: " + a);
+    //         Console.ReadLine();
+
+    //     }
+    //     static void DemoOut(out int value)
+    //     {
+    //         value = 5;
+    //         value += 5;
+
+    //         Console.WriteLine("Gia tri cua a sau khi thay doi la: " + value);
+    //     }
+    // }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SampleClass sc = new SampleClass();
+            IControl ctrl = new SampleClass();
+            ISurface srfc = new SampleClass();
+
+            sc.Paint();
+            ctrl.Paint();
+            srfc.Paint();
+        }
+
+        interface IControl
+        {
+            void Paint();
+        }
+        interface ISurface
+        {
+            void Paint();
+        }
+
+
+
+        public abstract class Human
+        {
+            String body = "xx";
+            String so_thich = "xx";
+            public abstract void eat();
+            public virtual string uongSua()
+            {
+                return "xxx";
+            }
+        }
+        class SampleClass : Human, IControl, ISurface
+        {
+            public override void eat()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Paint()
+            {
+                Console.WriteLine("Paint method in SampleClass");
+                Console.ReadKey();
+            }
+
+
+            void IControl.Paint()
+            {
+                Console.WriteLine("Paint method in IControl");
+                Console.ReadKey();
+            }
+
+            void ISurface.Paint()
+            {
+                Console.WriteLine("Paint method in ISurface");
+                Console.ReadKey();
+            }
+        }
+    }
+
+
+}
---
+++ CS009_LINQ/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace CS009_LINQ
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+    }
+    class Program
+    {
+        /// <summary>
+        /// Deferred or Lazy Operators:  These query operators are used for deferred execution. 
+        /// For example – select, SelectMany, where, Take, Skip, etc. are belongs to Deferred or Lazy Operators category.
+        /// Immediate or Greedy Operators: These query operators are used for immediate execution. 
+        /// For Example – count, average, min, max, First, Last, ToArray, ToList, etc. are belongs to the Immediate or Greedy Operators category.
+        /// </summary>
+        public static void Main()
+        {
+            List<Employee> listEmployees = new List<Employee>
+            {
+                new Employee { ID= 1001, Name = "Priyanka", Salary = 80000 },
+                new Employee { ID= 1002, Name = "Anurag", Salary = 90000 },
+                new Employee { ID= 1003, Name = "Preety", Salary = 80000 }
+            };
+            // In the below statement the LINQ Query is only defined and not executed
+            // If the query is executed here, then the result should not display Santosh
+            IEnumerable<Employee> result = from emp in listEmployees
+                                           where emp.Salary == 80000
+                                           select emp;
+            // Adding a new employee with Salary = 80000 to the collection listEmployees
+            listEmployees.Add(new Employee { ID = 1004, Name = "Santosh", Salary = 80000 });
+            // The LINQ query is actually executed when we iterate thru using a for each loop
+            // This is proved because Santosh is also included in the result
+            foreach (Employee emp in result)
+            {
+                Console.WriteLine($" {emp.ID} {emp.Name} {emp.Salary}");
+            }
+            Console.ReadKey();
+        }
+        // public static void Main()
+        // {
+        //     List<Employee> listEmployees = new List<Employee>
+        //     {
+        //         new Employee { ID= 1001, Name = "Priyanka", Salary = 80000 },
+        //         new Employee { ID= 1002, Name = "Anurag", Salary = 90000 },
+        //         new Employee { ID= 1003, Name = "Preety", Salary = 80000 }
+        //     };
+        //     // In the following statement, the LINQ Query is executed immediately as we are
+        //     // Using the ToList() method which is a greedy operator which forces the query 
+        //     // to be executed immediately
+        //     IEnumerable<Employee> result = (from emp in listEmployees
+        //                                    where emp.Salary == 80000
+        //                                    select emp).ToList();
+
+        //     // Adding a new employee with Salary = 80000 to the collection listEmployees
+        //     // will not have any effect on the result as the query is already executed
+        //     listEmployees.Add(new Employee { ID = 1004, Name = "Santosh", Salary = 80000 });
+
+        //     // The above LINQ query is executed at the time of its creation.
+        //     // This is proved because Santosh is not included in the result
+        //     foreach (Employee emp in result)
+        //     {
+        //         Console.WriteLine($" {emp.ID} {emp.Name} {emp.Salary}");
+        //     }
+        //     Console.ReadKey();
+        // }
+    }
+}
